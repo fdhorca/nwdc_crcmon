9fd321db44f9c1e45eab8e44dd09a697548912a8
Francis Horca <fdhorca@gmail.com>
2020-05-09 01:03:05 +0800

    Changes to be committed: 	new file:   crcmon_stack.yml 	modified:   initial_setup/setup.sh

--- initial_setup/setup.sh
+++ initial_setup/setup.sh
@@ -11,5 +11,7 @@ cat /etc/systemd/system/docker.service.d/http_proxy.conf
 systemctl daemon-reload
 systemctl restart docker
 
+docker network create --driver overlay crcmonNet
+
 docker image pull orangefolder/crcmon_flask:1.0
 docker image pull orangefolder/crcmon_nginx:1.0
