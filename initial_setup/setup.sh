c5cb584f79544fbeeb317625f1acdfeba72e1b67
Francis Horca <fdhorca@gmail.com>
2020-05-09 00:41:40 +0800

    Changes to be committed: 	modified:   initial_setup/setup.sh

--- initial_setup/setup.sh
+++ initial_setup/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-export http_proxy=muc1-prxy.sdi.trendnet.org:8080
+export http_proxy=sjc1-prxy.sdi.trendnet.org:8080
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+#systemctl start docker
 
 mkdir -p /etc/systemd/system/docker.service.d
-
 cp http_proxy.conf /etc/systemd/system/docker.service.d/http_proxy.conf
-
 cat /etc/systemd/system/docker.service.d/http_proxy.conf
-
 systemctl daemon-reload
+systemctl restart docker
+
+docker image pull orangefolder/crcmon_flask
+docker image pull orangefolder/crcmon_nginx
